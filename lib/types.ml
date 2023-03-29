dcac4b4e5f8ffeba4bafb425b262375df5cfb656
Hakimba <hakim.baaloudj@gmail.com>
2023-03-29 02:38:13 +0200

    simplified types

--- lib/types.ml
+++ lib/types.ml
@@ -7,13 +7,13 @@ type model =
   | GPT_3_5_TURBO
   | GPT_3_5_TURBO_0301
 
-let yojson_of_model = function
-  | GPT_4 -> `String "gpt-4"
-  | GPT_4_0314 -> `String "gpt-4-0314"
-  | GPT_4_32K -> `String "gpt-4-32k"
-  | GPT_4_32K_0314 -> `String "gpt-4-32k-0314"
-  | GPT_3_5_TURBO -> `String "gpt-3.5-turbo"
-  | GPT_3_5_TURBO_0301 -> `String "gpt-3.5-turbo-0301"
+let string_of_model = function
+  | GPT_4 -> {|"gpt-4"|}
+  | GPT_4_0314 -> {|"gpt-4-0314"|}
+  | GPT_4_32K -> {|"gpt-4-32k"|}
+  | GPT_4_32K_0314 -> {|"gpt-4-32k-0314"|}
+  | GPT_3_5_TURBO -> {|"gpt-3.5-turbo"|}
+  | GPT_3_5_TURBO_0301 -> {|"gpt-3.5-turbo-0301"|}
 
 let model_of_yojson = function
   | `String "gpt-4" -> Ok GPT_4
@@ -24,16 +24,17 @@ let model_of_yojson = function
   | `String "gpt-3.5-turbo-0301" -> Ok GPT_3_5_TURBO_0301
   | json -> Error (Yojson.Safe.to_string json)
 
-type role =
-  | User [@name "user"]
-  | Assistant [@name "assistant"]
-  | System [@name "system"]
-  [@@deriving yojson]
+let yojson_of_model v = v |> string_of_model |> Yojson.Safe.from_string
 
-let yojson_of_role = function
-  | User -> `String "user"
-  | Assistant -> `String "assistant"
-  | System -> `String "system"
+type role =
+  | User
+  | Assistant
+  | System
+
+let string_of_role = function
+  | User -> {|"user"|}
+  | Assistant -> {|"assistant"|}
+  | System -> {|"system"|}
 
 let role_of_yojson = function
   | `String "user" -> Ok User
@@ -41,7 +42,7 @@ let role_of_yojson = function
   | `String "system" -> Ok System
   | json -> Error (Yojson.Safe.to_string json)
 
-
+let yojson_of_role v = v |> string_of_role |> Yojson.Safe.from_string
 type messages = {
   role : role [@to_yojson yojson_of_role] [@of_yojson role_of_yojson];
   content : string 
